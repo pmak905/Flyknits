c7466f24ad185756327bdb59a70a8310c3da61d0
Eric Wong <eric.t.wong@cgi.com>
2017-07-25 22:21:36 -0400

    Open account
    Styling changes

--- app/src/main/java/com/hackabots/flyknits/NotificationsActivity.java
+++ app/src/main/java/com/hackabots/flyknits/NotificationsActivity.java
@@ -3,7 +3,10 @@ package com.hackabots.flyknits;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+
+import static com.hackabots.flyknits.MainActivity.EXTRA_HEADER;
 
 /**
  * Created by eric.wong on 2017-07-23.
@@ -18,5 +21,13 @@ public class NotificationsActivity extends AppCompatActivity {
         // Set page header
 //        Intent intent = getIntent();
 //        String header = intent.getStringExtra(MainActivity.EXTRA_HEADER);
+    }
+
+    public void launchOpenAccount(View view) {
+        Intent intent = new Intent(this, OpenAccountActivity.class);
+//        TextView text = (TextView) findViewById(R.id.chequing_title);
+//        String header = text.getText().toString();
+//        intent.putExtra(EXTRA_HEADER, header);
+        startActivity(intent);
     }
 }
